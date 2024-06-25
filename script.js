64afe81880b5d5b3e03bc3d0f2626b965cae81fc
Guillermocode23 <135448424+Guillermocode23@users.noreply.github.com>
2024-06-25 11:14:23 -0600

    Update script.js

--- script.js
+++ script.js
@@ -9,16 +9,5 @@ $(document).ready(function() {
             }, 800);
         }
     });
-    $(document).ready(function () {
-        // Smooth scroll to section on menu item click
-        $('.menu a').on('click', function (e) {
-            if (this.hash !== '') {
-                e.preventDefault();
-                const hash = this.hash;
-                $('html, body').animate({
-                    scrollTop: $(hash).offset().top
-                }, 800);
-            }
-        });
-    });
+   
 });
